1437475947244fa02edd9050b25a3cf93671c9a1
Jonas Hahn <herzock@gmail.com>
2024-02-04 13:37:34 +0100

    release build

---
+++ www/main.js
@@ -0,0 +1,124 @@
+import initTurbo, * as turbo from "/pkg/turbo_genesis_host_wasm_bindgen.js";
+
+/**************************************************/
+/* CONFIGURATION                                  */
+/**************************************************/
+
+// Game metadata
+const APP_NAME = "My Turbo Game";
+const APP_VERSION = "0.0.0";
+const APP_AUTHOR = "Leeroy Jenkins";
+const APP_DESCRIPTION = "An awesome game about awesomeness";
+
+// NOTE: You can find your builds in your rust crate in one of two places:
+// - target/wasm32-unknown-unknown/release/[package_name].wasm
+// - target/wasm32-unknown-unknown/debug/[package_name].wasm
+// Copy it into this web directory
+const WASM_SRC = "/my_game.wasm";
+
+// The game's resolution
+const RESOLUTION = [512, 320];
+
+// Add sprites to this array
+const SPRITES = [
+  "/sprites/pepe.png",
+  "/sprites/canon.png",
+  "/sprites/carrot.png",
+  "/sprites/clown.png",
+  "/sprites/lattice.png",
+  "/sprites/lauch.png",
+  "/sprites/manege.png",
+  "/sprites/onion.png",
+  "/sprites/pepe.png",
+  "/sprites/stove.png",
+  "/sprites/topf.png",
+  // Add as many as you have in your /sprites folder
+];
+
+/**************************************************/
+
+// This proxy prevents WebAssembly.LinkingError from being thrown
+// prettier-ignore
+window.createWasmImportsProxy = (target = {}) => {
+    console.log(target);
+    return new Proxy(target, {
+      get: (target, namespace) => {
+          // Stub each undefined namespace with a Proxy
+          target[namespace] = target[namespace] ?? new Proxy({}, {
+              get: (_, prop) => {
+                  // Generate a sub function for any accessed property
+                  return (...args) => {
+                      console.log(`Calling ${namespace}.${prop} with arguments:`, args);
+                      // Implement the actual function logic here
+                  };
+              }
+          });
+          return target[namespace];
+        }
+    })
+  };
+
+window.turboSolUser = window.turboSolUser ?? (() => null);
+window.turboSolGetAccount = window.turboSolGetAccount ?? (async () => {});
+window.turboSolSignAndSendTransaction =
+  window.turboSolSignAndSendTransaction ?? (async () => {});
+
+/**************************************************/
+
+try {
+  // Initalize Turbo's WASM runtime
+  await initTurbo();
+
+  // Create the game's canvas
+  const player = document.getElementById("player");
+
+  // Initialize a temporary 2D context canvas for loading state
+  const loading = document.createElement("canvas");
+  player?.appendChild(loading);
+  var context = loading.getContext("2d");
+  context.fillStyle = "white";
+  context.font = "bold 14px 04b03";
+  context.textAlign = "center";
+  context.textBaseline = "middle";
+  context.fillText("Loading...", loading.width / 2, loading.height / 2);
+
+  // Fetch sprites
+  const spriteData = await Promise.all(
+    SPRITES.map(async (src) => {
+      try {
+        let res = await fetch(src);
+        let buf = await res.arrayBuffer();
+        return [
+          src.replace(/^.*[\\/]/, "").replace(/.(png|jpg|jpeg|gif)$/, ""),
+          buf,
+        ];
+      } catch (err) {
+        console.error("Could not fetch sprite:", src);
+        return null;
+      }
+    }).filter((x) => !!x)
+  );
+
+  // Remove loading state
+  player?.removeChild(loading);
+
+  // Append game canvas
+  const canvas = document.createElement("canvas");
+  player?.appendChild(canvas);
+
+  // Run game
+  await turbo.run(canvas, spriteData, {
+    source: WASM_SRC,
+    meta: {
+      appName: APP_NAME,
+      appVersion: APP_VERSION,
+      appAuthor: APP_AUTHOR,
+      appDescription: APP_DESCRIPTION,
+    },
+    config: {
+      resolution: RESOLUTION,
+    },
+  });
+} catch (err) {
+  console.error("Turbo failed to initialize", err);
+}
---
+++ www/pkg/turbo_genesis_host_wasm_bindgen.d.ts
@@ -0,0 +1,125 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @param {HTMLCanvasElement} canvas
+* @param {Array<any>} sprites
+* @param {RunOptions} opts
+* @returns {Promise<void>}
+*/
+export function run(canvas: HTMLCanvasElement, sprites: Array<any>, opts: RunOptions): Promise<void>;
+export interface AppMetadata {
+    appAuthor: string | null;
+    appDescription: string | null;
+    appName: string | null;
+    appVersion: string | null;
+}
+
+export interface AppConfiguration {
+    fps: number | null;
+    resolution: [number, number] | null;
+    tileSize: [number, number] | null;
+}
+
+export interface RunOptions {
+    source: string;
+    meta: AppMetadata | null;
+    config: AppConfiguration | null;
+}
+
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly run: (a: number, b: number, c: number) => number;
+  readonly wgpu_compute_pass_set_pipeline: (a: number, b: number) => void;
+  readonly wgpu_compute_pass_set_bind_group: (a: number, b: number, c: number, d: number, e: number) => void;
+  readonly wgpu_compute_pass_set_push_constant: (a: number, b: number, c: number, d: number) => void;
+  readonly wgpu_compute_pass_insert_debug_marker: (a: number, b: number, c: number) => void;
+  readonly wgpu_compute_pass_push_debug_group: (a: number, b: number, c: number) => void;
+  readonly wgpu_compute_pass_pop_debug_group: (a: number) => void;
+  readonly wgpu_compute_pass_write_timestamp: (a: number, b: number, c: number) => void;
+  readonly wgpu_compute_pass_begin_pipeline_statistics_query: (a: number, b: number, c: number) => void;
+  readonly wgpu_compute_pass_end_pipeline_statistics_query: (a: number) => void;
+  readonly wgpu_compute_pass_dispatch_workgroups: (a: number, b: number, c: number, d: number) => void;
+  readonly wgpu_compute_pass_dispatch_workgroups_indirect: (a: number, b: number, c: number) => void;
+  readonly wgpu_render_bundle_set_pipeline: (a: number, b: number) => void;
+  readonly wgpu_render_bundle_set_bind_group: (a: number, b: number, c: number, d: number, e: number) => void;
+  readonly wgpu_render_bundle_set_vertex_buffer: (a: number, b: number, c: number, d: number, e: number) => void;
+  readonly wgpu_render_bundle_set_push_constants: (a: number, b: number, c: number, d: number, e: number) => void;
+  readonly wgpu_render_bundle_draw: (a: number, b: number, c: number, d: number, e: number) => void;
+  readonly wgpu_render_bundle_draw_indexed: (a: number, b: number, c: number, d: number, e: number, f: number) => void;
+  readonly wgpu_render_bundle_draw_indirect: (a: number, b: number, c: number) => void;
+  readonly wgpu_render_bundle_draw_indexed_indirect: (a: number, b: number, c: number) => void;
+  readonly wgpu_render_pass_set_pipeline: (a: number, b: number) => void;
+  readonly wgpu_render_pass_set_bind_group: (a: number, b: number, c: number, d: number, e: number) => void;
+  readonly wgpu_render_pass_set_vertex_buffer: (a: number, b: number, c: number, d: number, e: number) => void;
+  readonly wgpu_render_pass_set_push_constants: (a: number, b: number, c: number, d: number, e: number) => void;
+  readonly wgpu_render_pass_draw: (a: number, b: number, c: number, d: number, e: number) => void;
+  readonly wgpu_render_pass_draw_indexed: (a: number, b: number, c: number, d: number, e: number, f: number) => void;
+  readonly wgpu_render_pass_draw_indirect: (a: number, b: number, c: number) => void;
+  readonly wgpu_render_pass_draw_indexed_indirect: (a: number, b: number, c: number) => void;
+  readonly wgpu_render_pass_multi_draw_indirect: (a: number, b: number, c: number, d: number) => void;
+  readonly wgpu_render_pass_multi_draw_indexed_indirect: (a: number, b: number, c: number, d: number) => void;
+  readonly wgpu_render_pass_multi_draw_indirect_count: (a: number, b: number, c: number, d: number, e: number, f: number) => void;
+  readonly wgpu_render_pass_multi_draw_indexed_indirect_count: (a: number, b: number, c: number, d: number, e: number, f: number) => void;
+  readonly wgpu_render_pass_set_blend_constant: (a: number, b: number) => void;
+  readonly wgpu_render_pass_set_scissor_rect: (a: number, b: number, c: number, d: number, e: number) => void;
+  readonly wgpu_render_pass_set_viewport: (a: number, b: number, c: number, d: number, e: number, f: number, g: number) => void;
+  readonly wgpu_render_pass_set_stencil_reference: (a: number, b: number) => void;
+  readonly wgpu_render_pass_insert_debug_marker: (a: number, b: number, c: number) => void;
+  readonly wgpu_render_pass_push_debug_group: (a: number, b: number, c: number) => void;
+  readonly wgpu_render_pass_pop_debug_group: (a: number) => void;
+  readonly wgpu_render_pass_write_timestamp: (a: number, b: number, c: number) => void;
+  readonly wgpu_render_pass_begin_pipeline_statistics_query: (a: number, b: number, c: number) => void;
+  readonly wgpu_render_pass_end_pipeline_statistics_query: (a: number) => void;
+  readonly wgpu_render_pass_execute_bundles: (a: number, b: number, c: number) => void;
+  readonly wgpu_render_bundle_set_index_buffer: (a: number, b: number, c: number, d: number, e: number) => void;
+  readonly wgpu_render_bundle_pop_debug_group: (a: number) => void;
+  readonly wgpu_render_bundle_insert_debug_marker: (a: number, b: number) => void;
+  readonly wgpu_render_bundle_push_debug_group: (a: number, b: number) => void;
+  readonly wgpu_render_pass_set_index_buffer: (a: number, b: number, c: number, d: number, e: number) => void;
+  readonly __wbindgen_malloc: (a: number, b: number) => number;
+  readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+  readonly __wbindgen_export_2: WebAssembly.Table;
+  readonly wasm_bindgen__convert__closures__invoke8__h749ba1736476823b: (a: number, b: number, c: number, d: number, e: number, f: number, g: number, h: number, i: number, j: number) => void;
+  readonly wasm_bindgen__convert__closures__invoke1_mut__h089b9cc2791d6346: (a: number, b: number, c: number) => void;
+  readonly wasm_bindgen__convert__closures__invoke2__h3b7f84c79c4ac2c8: (a: number, b: number, c: number, d: number) => void;
+  readonly wasm_bindgen__convert__closures__invoke2__h77cb007ec23deefa: (a: number, b: number, c: number, d: number) => number;
+  readonly _dyn_core__ops__function__Fn_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h66a8f087c40a3901: (a: number, b: number) => number;
+  readonly wasm_bindgen__convert__closures__invoke6__hf8d8ef438075de83: (a: number, b: number, c: number, d: number, e: number, f: number, g: number, h: number) => number;
+  readonly wasm_bindgen__convert__closures__invoke1__h2d17859fc2302ea8: (a: number, b: number, c: number) => number;
+  readonly wasm_bindgen__convert__closures__invoke0_mut__h62144c8549583437: (a: number, b: number) => void;
+  readonly wasm_bindgen__convert__closures__invoke2_mut__h61d75cf130769dc9: (a: number, b: number, c: number, d: number) => void;
+  readonly wasm_bindgen__convert__closures__invoke6__heb35c5c87b83ced0: (a: number, b: number, c: number, d: number, e: number, f: number, g: number, h: number) => void;
+  readonly wasm_bindgen__convert__closures__invoke5__h156eabf381290da9: (a: number, b: number, c: number, d: number, e: number, f: number, g: number) => void;
+  readonly wasm_bindgen__convert__closures__invoke1__h57b7f514cff7fb4f: (a: number, b: number, c: number) => void;
+  readonly wasm_bindgen__convert__closures__invoke4__h1127e1d7bb001a30: (a: number, b: number, c: number, d: number, e: number, f: number) => void;
+  readonly wasm_bindgen__convert__closures__invoke0_mut__h6ef0fbeb3e682c2c: (a: number, b: number) => void;
+  readonly wasm_bindgen__convert__closures__invoke1_mut__h0988869b23b4b48d: (a: number, b: number, c: number) => void;
+  readonly _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hfdf2c89756a6bdc1: (a: number, b: number, c: number) => void;
+  readonly __wbindgen_free: (a: number, b: number, c: number) => void;
+  readonly __wbindgen_exn_store: (a: number) => void;
+  readonly wasm_bindgen__convert__closures__invoke2_mut__h6b34c662dfd3bf3c: (a: number, b: number, c: number, d: number) => void;
+}
+
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+/**
+* Instantiates the given `module`, which can either be bytes or
+* a precompiled `WebAssembly.Module`.
+*
+* @param {SyncInitInput} module
+*
+* @returns {InitOutput}
+*/
+export function initSync(module: SyncInitInput): InitOutput;
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {InitInput | Promise<InitInput>} module_or_path
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function __wbg_init (module_or_path?: InitInput | Promise<InitInput>): Promise<InitOutput>;
